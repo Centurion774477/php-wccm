39630a2d80fa63372dfbe683a3bd110ac9b644cd
Reece Turner <reeceturner1358@gmail.com>
2026-09-12 19:46:08 -0400

    fixed

--- wccm.php
+++ wccm.php
@@ -3,112 +3,103 @@
 declare(strict_types=1);
 
 getfile:
-echo "WCCM Re-implementation. Please provide a file:";
+echo "\nPHP-WCCM. Please provide a file (type 'exit' to leave):" . PHP_EOL;
 $file = readline();
+
+if ($file === "exit") {
+    exit;
+}
 
 if ($file == "" || $file == null ) {
     echo "Invalid file provided: your input was null.";
     goto getfile;
 }
 
-$table = [];
-
-# very primitive verification for now
-function verifyFile(array $lines): array {
-    foreach ($lines as $line) {
-        $index = 0;
-        if ($index == 0) {
-            if (!preg_match("/(.*)\s+|\s+(.*)\s+|/", $line)) {
-                return [
-                    false, 
-                    1, 
-                    <<<EOT
-                    Line one is dedicated for the word and the pronunciation.
-                    It should look like:
-                    drink | dɹINkH |
-                    EOT
-                ];
-            }
-        } elseif ($index == 2) {
-            if (!preg_match("/\w+\s+\w+/", $line)) {
-                return [
-                    false, 
-                    2, 
-                    <<<EOT
-                    The second line is dedicated for the parts of speech.
-                    For example:
-                    noun verb
-                    EOT
-                ];
-            }
-        } elseif ($index == 3) {
-            if (!preg_match("/\w+\s+\w+/", $line)) {
-                return [
-                    false, 
-                    3, 
-                    <<<EOT
-                    The third line is for the plural form of the word.
-                    It should look like this:
-                    plural drinks
-                    EOT
-                ];
-            }
-        }
-        $index += 1;
-    }
-    return [true, ""];
+if (!file_exists($file)) {
+    echo "The given file: $file does not exist in this context.";
+    goto getfile;
 }
 
+$consumingMeanings = false;
+$meanings = [];
+
+$consumingSynonyms = false;
+$synonyms = [];
+
+$consumingAntonyms = false;
+$antonyms = [];
+
+$table = [
+    "word"            => "",
+    "pronunciation"   => "",
+    "parts_of_speech" => "",
+    "plural_form"     => "",
+    "meanings"        => [],
+    "synonyms"        => [],
+    "antonyms"        => []
+];
 
 function lex(string $line, int $index): void {
+    global $consumingMeanings, $consumingSynonyms, $consumingAntonyms, $table;
+
     if ($index == 1) {
-        $tuple = explode("", $line);
-        array_push($table, [
-        "word" => $tuple[0],
-        "pronunciation" => $tuple[1],
-        ]);
+        $tuple = array_map('trim', explode('|', $line));
+        $table["word"] = $tuple[0];
+        $table["pronunciation"] = $tuple[1];
+        return;
     } elseif ($index == 2) {
-        $partsOfSpeech = explode("", $line);
-        array_push($table, [
-        "parts_of_speech" => $partsOfSpeech
-        ]);
+        $table["parts_of_speech"] = $line;
+        return;
     } elseif ($index == 3) {
-        $pluralForm = explode("plural", $line);
-        array_push($table, [
-            "plural_form"=> $pluralForm[1],
-        ]);
-    } elseif ($index == 5) {
-        $meanings = [];
-        while ($line != "--") {
-            array_push($meanings, $line);
-        }
-        array_push($table, $meanings);
-    } elseif ($line == "--") {
-        $synonyms = [];
-        while ($line != "~~") {
-            array_push($synonyms, $line);
-        }
-        array_push($table, $synonyms);
-    } elseif ($line == "~~") {
-        $antonyms = [];
-        while ($line != "" && $line != null) {
-            array_push($antonyms, $line);
-        }
-        array_push($table, $antonyms);
+        $table["plural_form"] = trim($line);
+    }
+    // elseif ($index == 4) {
+    //     $pluralForm = explode("plural", $line);
+    //     # $table["plural_form"] = trim($pluralForm[1] ?? '');
+
+    //     echo "Plural form:";
+    //     echo(trim($pluralForm[1] ?? ''));
+
+    //     $consumingMeanings = true;
+    //     $consumingSynonyms = false;
+    //     $consumingAntonyms = false;
+    //     return;
+    // }
+
+    if ($line == "--") {
+        $consumingMeanings = false;
+        $consumingSynonyms = true;
+        return;
+    } 
+    if ($line == "~~") {
+        $consumingSynonyms = false;
+        $consumingAntonyms = true;
+        return;
+    }
+
+    if ($consumingMeanings && $line != "") {
+        $table["meanings"][] = $line;
+    } elseif ($consumingSynonyms && $line != "") {
+        $table["synonyms"][] = $line;
+    } elseif ($consumingAntonyms && $line != "") {
+        $table["antonyms"][] = $line;
     }
 }
 
-function generate(array $table): void {
+function generate(): void {
+    global $table;
     try {
         $handle = fopen("wccm.md", "w");
         if ($handle) {
-            fwrite($handle, "{$table["word"]} | {$table["pronunciation"]} |" . PHP_EOL);
-            fwrite($handle, "{$table["parts_of_speech"]}". PHP_EOL);
-            fwrite($handle, "Plural: {$table["plural_form"]}". PHP_EOL);
+            fwrite($handle, "{$table["word"]} | {$table["pronunciation"]} | {$table["parts_of_speech"]}" . PHP_EOL);
+
+            $plural_form = ucfirst($table["plural_form"]);
+            $plural = preg_replace("/^Plural/", "Plural:", $plural_form);
+            fwrite($handle, $plural . PHP_EOL);
 
             fwrite($handle, PHP_EOL);
 
-            fwrite($handle, "Meanings". PHP_EOL);
+            fwrite($handle, "Meanings:". PHP_EOL);
             foreach ($table["meanings"] as $meaning) {
                 fwrite($handle, "  -$meaning" . PHP_EOL);
             }
@@ -134,16 +125,13 @@ function generate(array $table): void {
 $fileContent = file_get_contents($file);
 $lines = explode(PHP_EOL, $fileContent);
 
-$verification = verifyFile($lines);
-if ($verification[0] == false) {
-    echo "Your file does not follow the conventions.";
-    echo "Broken line: {$verification[1]}";
-    echo $verification[2];
-    exit;
-}
 
+$index = 1;
 foreach ($lines as $line) {
-    $index = 1;
     lex($line, $index);
     $index += 1;
 }
+
+generate();
+
+echo "Thank you for using PHP-WCCM. Your output can be seen in wccm.md";
